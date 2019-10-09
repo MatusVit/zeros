214b896bc2eb8467aa6b5ffccfe805949b330378
MatusVit <matvi@mail.ru>
2019-10-09 17:15:31 +0300

    init: start first save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-module.exports = function zeros(expression) {
+// module.exports = function zeros(expression) {
+//   // your solution
+// }
+
+
+function zeros(expression) {
   // your solution
+  
+
+
 }
+
+function countZeroFact (number) {
+  
+  let sumZeros = 0;
+
+  if(number > 4) sumZeros += Math.floor(number/5);
+  if(number > 24) sumZeros += Math.floor(number/25);
+  if(number > 124) sumZeros += Math.floor(number/125);
+
+  return sumZeros;  
+} 
+
+
+
+let step = 0;
+function countZeroDoubFact (number) {
+  console.log("Вызов начало SETP- " + ++step + ", number - " + number);
+  
+  let sumZeros = 0;
+
+  if(number != 0) {
+    // sumZeros = countZeroFact(number) - countZeroDoubFact(number - 1); 
+    sumZeros = countZeroFact(number) - countZeroDoubFact(number - 1); 
+
+
+
+    console.log("выход STEP- " + step-- + ", sumZeros - " + sumZeros);
+  }
+  return sumZeros;  
+} 
+
+// let testStr = '100';
+// console.log("рузультат ! = " + countZeroFact (testStr));
+
+
+ let testStr2 = '22';
+console.log("рузультат !! = " + countZeroDoubFact (testStr2));
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// testStr = '';
+// console.log("рузультат = " + zeros (testStr));
+ 
